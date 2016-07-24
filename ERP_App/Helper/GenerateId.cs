4d5daac2857175ba5fd0bcbd0d8a4262aa060eac
Juan Jose Soriano <jjsorianoe@gmail.com>
2016-07-24 16:28:15 -0500

    Connection to the Data Moddel

---
+++ ERP_App/Helper/GenerateId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERP_App.Helper
+{
+    public class GenerateId
+    {
+        public string generateID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
