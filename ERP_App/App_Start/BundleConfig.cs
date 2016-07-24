e9ad834e463169659ffeb8577606b8396e9ecffd
nesticle8bit <juliojafeth2000_@hotmail.com>
2016-07-24 15:49:11 -0500

    Cambios en el bundle, se agrego el font awesome

--- ERP_App/App_Start/BundleConfig.cs
+++ ERP_App/App_Start/BundleConfig.cs
@@ -13,9 +13,7 @@ namespace ERP_App
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -23,9 +21,15 @@ namespace ERP_App
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //CSS Styles
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
+                "~/Content/font-awesome.min.css"
+                ));
         }
     }
 }
