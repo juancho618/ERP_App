cc499b4ec09ed686a5daeab783d114cd0224a6d2
Juan Jose Soriano <jjsorianoe@gmail.com>
2016-07-24 14:37:16 -0500

    Add project files.

---
+++ ERP_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERP_App.Startup))]
+namespace ERP_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
